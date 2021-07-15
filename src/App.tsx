2b3a949ef67d7f28a16665c72b7088271f4509fd
MuriloMarquesSantos <murilommms@gmail.com>
2021-07-15 07:51:38 -0300

    Adding toast styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -12,6 +13,8 @@ const App: React.FC = () => (
         <Routes />
       </BrowserRouter>
     </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -40,9 +40,10 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (error) {
-        const errors = getValidationErrors(error);
-        formRef.current?.setErrors(errors);
-        console.error(error);
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
